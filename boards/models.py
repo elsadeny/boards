3f0e08412022033df2e46dffc26407634d280f87
elsadeny <alphasadeny@gmail.com>
2019-03-03 20:12:56 +0800

    changing repo

--- boards/models.py
+++ boards/models.py
@@ -8,6 +8,8 @@ from django.utils.text import Truncator
 from markdown import markdown
 
 
+
+
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
@@ -25,8 +27,8 @@ class Board(models.Model):
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
-    board = models.ForeignKey(Board, related_name='topics')
-    starter = models.ForeignKey(User, related_name='topics')
+    board = models.ForeignKey(Board, related_name='topics',on_delete=models.CASCADE)
+    starter = models.ForeignKey(User, related_name='topics',on_delete=models.CASCADE)
     views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
@@ -51,14 +53,13 @@ class Topic(models.Model):
     def get_last_ten_posts(self):
         return self.posts.order_by('-created_at')[:10]
 
-
 class Post(models.Model):
     message = models.TextField(max_length=4000)
-    topic = models.ForeignKey(Topic, related_name='posts')
+    topic = models.ForeignKey(Topic, related_name='posts',on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, related_name='posts')
-    updated_by = models.ForeignKey(User, null=True, related_name='+')
+    created_by = models.ForeignKey(User, related_name='posts',on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, null=True, related_name='+',on_delete=models.CASCADE)
 
     def __str__(self):
         truncated_message = Truncator(self.message)
